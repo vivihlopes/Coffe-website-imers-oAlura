aad410b91352c91f170520686fac2572668fadf8
Vitória Nascimento Lopes <128759612+vivihlopes@users.noreply.github.com>
2024-09-07 23:46:58 -0300

    Add files via upload

---
+++ Imersão/dados.js
@@ -0,0 +1,98 @@
+let dados = [
+    {
+      imagem: "img/capuccino.png",
+      imagem_descricao: "Imagem do cappucino",
+      titulo: "Cappuccino",
+      descricao: "Cappuccino, Um clássico italiano, feito com espresso, leite vaporizado e espuma de leite. Perfeito para começar o dia.",
+      link: "https://perfectdailygrind.com/2020/10/what-is-a-mocha/",
+      tags:"italiano, vaporizado, expresso, leite, espuma"
+    },
+
+    {
+      imagem: "img/affogado.png",
+      imagem_descricao: "Imagem de um affogato",
+      titulo: "Affogato",
+      descricao: "Uma sobremesa italiana clássica, preparada com uma bola de sorvete coberta com espresso quente. Uma combinação simples e deliciosa.",
+      link: "https://www.seriouseats.com/recipes/20110524-affogato-recipe",
+      tags: "italiano, espresso, sorvete",
+  },
+
+    {
+      imagem: "img/icecramelo.png",
+      imagem_descricao: "Imagem de um iced caramel latte",
+      titulo: "Iced Caramel Latte",
+      descricao: "Um clássico americano com um toque adocicado. Espresso, leite frio e um generoso shot de calda de caramelo. Perfeito para os dias quentes.",
+      link: "https://www.starbucks.com/menu/drinks/iced-coffees",
+      tags: "americano, caramelo, leite, gelado, doce"
+  },
+
+  {
+    imagem: "img/mochalate.png",
+    imagem_descricao: "Imagem de um mocha latte",
+    titulo: "Mocha Latte",
+    descricao: "A combinação perfeita de chocolate e café. Espresso, leite vaporizado, chocolate e, opcionalmente, chantilly. Um clássico para os amantes de chocolate.",
+    link: "https://perfectdailygrind.com/2020/10/what-is-a-mocha/",
+    tags: "chocolate, espresso, leite, vaporizado, doce"
+},
+{
+  imagem: "img/icedcappucino.png",
+  imagem_descricao: "Imagem de um iced cappuccino",
+  titulo: "Iced Cappuccino",
+  descricao: "A versão gelada do clássico italiano. Espresso, leite frio e espuma de leite. Refrescante e cremoso.",
+  link: "https://www.nescafe.com/br/receitas/iced-caramel-latte",
+  tags: "italiano, espresso, leite, espuma, gelado"
+},
+
+{
+  imagem: "img/graosdecafe1.png",
+  imagem_descricao: "Imagem de grãos de café arábica",
+  titulo: "Grãos de Café Arábica",
+  descricao: "Conhecidos por seu sabor suave e aroma floral, os grãos arábica são os mais populares do mundo. Perfeitos para cafés mais leves e frutados.",
+  link: "https://www.cafespelomundo.com/cafe-arabica/",
+  tags: "arabica, suave, floral, frutado"
+},
+{
+  imagem: "img/graosdecafe2.png",
+  imagem_descricao: "Imagem de grãos de café house blend",
+  titulo: "House Blend",
+  descricao: "Uma mistura equilibrada de diferentes grãos, oferecendo um sabor rico e complexo. Ideal para o dia a dia.",
+  link: "https://www.nespresso.com/br/pt/coffee/house-blends",
+  tags: "blend, equilibrado, rico, complexo"
+},
+
+{
+  imagem: "img/graosdecafe3.png",
+  imagem_descricao: "Imagem de grãos de café robusta",
+  titulo: "Grãos de Café Robusta",
+  descricao: "Com sabor mais forte e com maior teor de cafeína, os grãos robusta são ideais para expressos e bebidas com sabor mais intenso.",
+  link: "https://www.cafespelomundo.com/cafe-robusta/",
+  tags: "robusta, forte, cafeina, intenso"
+},
+
+    {
+      imagem: "img/cold_java.png",
+      imagem_descricao: "Imagem do Cold java",
+      titulo: "Cold Java",
+      descricao: "Cold Java, café frio concentrado e saboroso, perfeito para os dias quentes.",
+      link: "https://en.wikipedia.org/wiki/Cold_brew_coffee",
+      tags:"cold brew, brew,frio, café, refrescante, verão"
+    },
+
+    {
+      imagem: "/img/mocha.png",
+      imagem_descricao: "Imagem do mocha",
+      titulo: "Mocha",
+      descricao: "Mocha, um clássico com chocolate, espresso e leite vaporizado.",
+      link: "https://perfectdailygrind.com/2020/10/what-is-a-mocha/",
+       tags:"chocolate, expresso, doce,leite, gourmet, bebida quente"
+    },
+
+    {
+      imagem: "img/latte.png",
+      imagem_descricao: "Imagem do latte",
+      titulo: "Latte",
+      descricao: "O latte é um café italiano cremoso e suave. Ele é feito com espresso e leite vaporizado, formando uma espuma aveludada. ",
+      link: "https://perfectdailygrind.com/2020/10/what-is-a-mocha/",
+      tags:"italiano,leite, suave, cremoso, café da manhã"
+    },
+  ];
